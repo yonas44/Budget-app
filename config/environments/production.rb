ce92d57ebeb666c675425ce70600218c930a5f11
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-24 11:19:00 +0300

    Add host for my app to link to in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer handler for user registeration
+  config.action_mailer.default_url_options = { host: 'https://budget-app-o4fw.onrender.com' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
